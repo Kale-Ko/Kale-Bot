fbbab4cbcc23963d25d2241227c801cf5b58715d
Kale-Ko <info@kaleko.ga>
2021-04-19 12:59:03 -0400

    Update

--- bot.js
+++ bot.js
@@ -2,17 +2,17 @@ const Discord = require('discord.js')
 const client = new Discord.Client()
 const fs = require('fs')
 
-var data
+var data = {}
 
-fs.stat("data.json", function (err, stats) {
+fs.stat("./data.json", function (err, stats) {
     if (err) return
 
-    fs.readFile("data.json", "utf8", (err, newData) => {
+    fs.readFile("./data.json", "utf8", (err, newData) => {
         data = JSON.parse(newData)
 
         data.lastDowntimeReason = "a crash"
 
-        fs.writeFileSync("data.json", JSON.stringify(data, null, 4))
+        fs.writeFileSync("./data.json", JSON.stringify(data, null, 4))
 
         client.on('ready', () => {
             console.log("Bot Logged in as " + client.user.tag)
@@ -21,7 +21,7 @@ fs.stat("data.json", function (err, stats) {
         })
 
         client.on('message', message => {
-            var config = data.servers[message.guild.id]
+            if (message.channel.type != "dm") var config = data.servers[message.guild.id]; else var config = { prefix: "?", deleteTimeout: 2147483.647, atSender: false }
 
             if (!message.author.bot && message.content.startsWith(config.prefix)) handleCommand(message, config)
 
@@ -70,7 +70,10 @@ fs.stat("data.json", function (err, stats) {
             var args = message.content.toLowerCase().split(" "); args.shift()
 
             if (command == "help") {
-                sendEmbed(message.channel, message.author, config, "Help", "**Unranked**\nUse " + config.prefix + "help to show this\nUse " + config.prefix + "about to get info about the bot\nUse " + config.prefix + "uptime to get how long the bot has been online\n\n**Admins**\nUse " + config.prefix + "clear to clear a channels messages\nUse " + config.prefix + "prefix to set the prefix\nUse " + config.prefix + "deletetimeout to set the delete timeout\nUse " + config.prefix + "atsender to change whether it ats the sender of the message")
+                var help = "**Unranked**\nUse " + config.prefix + "help to show this\nUse " + config.prefix + "about to get info about the bot\nUse " + config.prefix + "uptime to get how long the bot has been online"
+                if (message.channel.type != "dm") help += "\n\n**Admins**\nUse " + config.prefix + "clear to clear a channels messages\nUse " + config.prefix + "prefix to set the prefix\nUse " + config.prefix + "deletetimeout to set the delete timeout\nUse " + config.prefix + "atsender to change whether it ats the sender of the message"
+
+                sendEmbed(message.channel, message.author, config, "Help", help)
             } else if (command == "about") {
                 sendEmbed(message.channel, message.author, config, "About", "Kale bot is a miscellaneous Discord bot made by <@622242352433725451>")
             } else if (command == "uptime") {
@@ -81,7 +84,7 @@ fs.stat("data.json", function (err, stats) {
                 var differentSeconds = Math.ceil(difference / 1000 - 1) - ((differentMinutes * 60) + (differentHours * 60 * 60) + (differentDays * 24 * 60 * 60))
 
                 sendEmbed(message.channel, message.author, config, "Uptime", "The bot has been online for " + differentDays + " days, " + differentHours + " hours, and " + differentMinutes + " minutes and " + differentSeconds + " seconds\nIt last went offline because of " + data.lastDowntimeReason)
-            } else if (command == "clear") {
+            } else if (command == "clear" && message.channel.type != "dm") {
                 if (message.member.hasPermission('MANAGE_MESSAGES', { checkAdmin: true, checkOwner: true })) {
                     var amount = args[0]
                     if (amount == undefined || amount == null || amount == "") amount = 100
@@ -96,32 +99,44 @@ fs.stat("data.json", function (err, stats) {
                 } else {
                     sendEmbed(message.channel, message.author, config, "Denied", "You do not have the permission to do that", "https://static.thenounproject.com/png/372212-200.png")
                 }
-            } else if (command == "prefix") {
-                if (args[0] == undefined || args[0] == "") { sendEmbed(message.channel, message.author, config, "Invalid", "That is not a valid prefix"); return }
+            } else if (command == "prefix" && message.channel.type != "dm") {
+                if (message.member.hasPermission('MANAGE_SERVER', { checkAdmin: true, checkOwner: true })) {
+                    if (args[0] == undefined || args[0] == "") { sendEmbed(message.channel, message.author, config, "Invalid", "That is not a valid prefix"); return }
 
-                config.prefix = args[0]
+                    config.prefix = args[0]
 
-                fs.writeFileSync("data.json", JSON.stringify(data, null, 4))
+                    fs.writeFileSync("data.json", JSON.stringify(data, null, 4))
 
-                sendEmbed(message.channel, message.author, config, "Successfully set", "Successfully set the prefix to " + args[0])
-            } else if (command == "deletetimeout") {
-                if (args[0] == undefined || args[0] == "") { sendEmbed(message.channel, message.author, config, "Invalid", "That is not a valid amount"); return }
+                    sendEmbed(message.channel, message.author, config, "Successfully set", "Successfully set the prefix to " + args[0])
+                } else {
+                    sendEmbed(message.channel, message.author, config, "Denied", "You do not have the permission to do that", "https://static.thenounproject.com/png/372212-200.png")
+                }
+            } else if (command == "deletetimeout" && message.channel.type != "dm") {
+                if (message.member.hasPermission('MANAGE_SERVER', { checkAdmin: true, checkOwner: true })) {
+                    if (args[0] == undefined || args[0] == "") { sendEmbed(message.channel, message.author, config, "Invalid", "That is not a valid amount"); return }
 
-                config.deleteTimeout = parseFloat(args[0])
+                    config.deleteTimeout = parseFloat(args[0])
 
-                fs.writeFileSync("data.json", JSON.stringify(data, null, 4))
+                    fs.writeFileSync("data.json", JSON.stringify(data, null, 4))
 
-                sendEmbed(message.channel, message.author, config, "Successfully set", "Successfully set the delete timout to " + args[0])
-            } else if (command == "atsender") {
-                if (args[0] != "true" && args[0] != true && args[0] == "false" && args[0] == false) { sendEmbed(message.channel, message.author, config, "Invalid", "That is not a valid bollean"); return }
+                    sendEmbed(message.channel, message.author, config, "Successfully set", "Successfully set the delete timout to " + args[0])
+                } else {
+                    sendEmbed(message.channel, message.author, config, "Denied", "You do not have the permission to do that", "https://static.thenounproject.com/png/372212-200.png")
+                }
+            } else if (command == "atsender" && message.channel.type != "dm") {
+                if (message.member.hasPermission('MANAGE_SERVER', { checkAdmin: true, checkOwner: true })) {
+                    if (args[0] != "true" && args[0] != true && args[0] == "false" && args[0] == false) { sendEmbed(message.channel, message.author, config, "Invalid", "That is not a valid bollean"); return }
 
-                var value = true; if (args[0] == "false" || args[0] == false) value = false
+                    var value = true; if (args[0] == "false" || args[0] == false) value = false
 
-                config.atSender = value
+                    config.atSender = value
 
-                fs.writeFileSync("data.json", JSON.stringify(data, null, 4))
+                    fs.writeFileSync("data.json", JSON.stringify(data, null, 4))
 
-                sendEmbed(message.channel, message.author, config, "Successfully set", "Successfully set at sender to " + value)
+                    sendEmbed(message.channel, message.author, config, "Successfully set", "Successfully set at sender to " + value)
+                } else {
+                    sendEmbed(message.channel, message.author, config, "Denied", "You do not have the permission to do that", "https://static.thenounproject.com/png/372212-200.png")
+                }
             } else {
                 sendEmbed(message.channel, message.author, config, "Unknown Command", "That is not a command, use " + config.prefix + "help for a list of commands")
             }
