b35f37f36ec552170bd84633763bae6aaa947a84
Kale-Ko <info@kaleko.ga>
2021-05-12 10:59:52 -0400

    Update

--- slashCommands/help.js
+++ slashCommands/help.js
@@ -7,7 +7,7 @@ module.exports = {
     slash: true,
     callback: ({ message, channel, args, text, client, prefix, instance, interaction }) => {
         var help = "**Unranked**\nUse /help to show this\nUse /about to get info about the bot\nUse /uptime to get how long the bot has been online"
-        if (channel.type != "dm") help += "\n\n**Admins**\nUse /clear to clear a channels messages\nUse /prefix to set the prefix\nUse /deletetimeout to set the delete timeout\nUse /atsender to change whether it ats the sender of the message"
+        if (message.channel.type != "dm") help += "\n\n**Admins**\nUse /clear to clear a channels messages\nUse /prefix to set the prefix\nUse /deletetimeout to set the delete timeout\nUse /atsender to change whether it ats the sender of the message"
 
         return createEmbed("Help", help)
     }
