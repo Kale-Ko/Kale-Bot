bb6f53768a438a26af2e84dd25f35eed86f6b139
Kale-Ko <github@kaleko.ga>
2021-05-26 19:27:49 -0400

    Update

--- commands/config.js
+++ commands/config.js
@@ -1,14 +1,10 @@
-const { sendEmbed } = require("../util.js")
-
 module.exports = {
     name: "config",
     description: "Get info about the bot",
-    category: "Info",
-    subCommands: [{ name: "get", description: "Get all the current config", paramiters: [], requiredPermissions: [], }],
+    category: "Management",
+    subCommands: [{ name: "get", description: "Get all the current config", paramiters: ["name"], requiredPermissions: [], }],
     paramiters: [],
     requiredPermissions: [],
     worksInDms: false,
-    callback: (message, args, client, config) => {
-        
-    }
+    callback: (message, args, client, config) => { }
 }
