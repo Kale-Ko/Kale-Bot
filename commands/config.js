8e121d6e9d05690758d7e50322cfb9965ef42d55
Kale Ko <github@kaleko.ga>
2021-05-28 13:47:52 -0400

    Update

--- commands/config.js
+++ commands/config.js
@@ -5,7 +5,7 @@ module.exports = {
     name: "config",
     description: "Get info about the bot",
     category: "Management",
-    paramiters: "(Get, Set) {Key} {Value (Set)}",
+    paramiters: "(Get, Set) {Key (Set)} {Value (Set)}",
     requiredPermissions: ["MANAGE_SERVER"],
     worksInDms: false,
     callback: (message, args, client, config) => {
@@ -25,7 +25,20 @@ module.exports = {
             
             sendEmbed(message.channel, message.author, config, "Config", helpText)
         } else if (args[0] == "set") {
+            if (args.length < 3) { var newMessage = message; newMessage.content = config.prefix + "help " + module.exports.name; runCommand(newMessage, config); return }
             
+            var keys = args[1].split(".")
+            var value = args[2]
+            
+            var configKey = config
+            
+            keys.forEach(key => {
+                if (key != keys[keys.length - 1]) configKey = configKey[key]
+            })
+            
+            fs.writeFileSync("../config.json", config)
+            
+            configKey = value
         } else {
             var newMessage = message
             newMessage.content = config.prefix + "help " + module.exports.name
