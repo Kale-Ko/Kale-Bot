477bbf6465ccaaf2607ee9775937935e0885673c
Kale Ko <github@kaleko.ga>
2021-05-28 12:42:23 -0400

    Update

--- commands/config.js
+++ commands/config.js
@@ -1,3 +1,5 @@
+const { runCommand } = require("../features/commands.js")
+
 module.exports = {
     name: "config",
     description: "Get info about the bot",
@@ -6,6 +8,16 @@ module.exports = {
     requiredPermissions: [],
     worksInDms: false,
     callback: (message, args, client, config) => {
-        
+        if (args[0] == "get") {
+            for (var key of Object.keys(config)) {
+                
+            }
+        } else if (args[0] == "set") {
+            
+        } else {
+            var newMessage = message
+            newMessage.content = config.prefix + "help " + this.name
+            runCommand(newMessage, config)
+        }
     }
 }
