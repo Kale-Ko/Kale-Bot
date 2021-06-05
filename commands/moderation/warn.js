08b51d38b8116bcdb55544d8dd436fd0ad58f5cf
Kale Ko <github@kaleko.ga>
2021-06-05 11:46:37 -0400

    Update

---
+++ commands/moderation/warn.js
@@ -0,0 +1,26 @@
+const fs = require("fs")
+const { data } = require("../../bot.js")
+const { createEmbed, sendEmbed } = require("../../util.js")
+
+module.exports = {
+    name: "warn",
+    description: "Warn a person",
+    paramiters: [{ type: "paramiter", name: "user", optional: false }, { type: "paramiter", name: "reason", optional: false }],
+    requiredPermissions: ["MANAGE_MESSAGES"],
+    worksInDms: false,
+    callback: (message, args, client, config) => {
+        message.guild.members.fetch(args[0].replace("<@!", "").replace("<@", "").replace(">", "")).then(user => {
+            user.send(createEmbed("Warned", "You have been warned in " + user.guild.name + " for " + (args[1] || ""))).then(() => {
+                sendEmbed(message.channel, message.author, config, "Warned", "Successfully warned <@" + user.id + "> for " + (args[1] || ""))
+                
+                if (data.logs[message.guild.id].warns[user.id] == null) data.logs[message.guild.id].warns[user.id] = []
+                
+                data.logs[message.guild.id].warns[user.id].push({ "by": message.author.id, "against": user.id, "for": args[1] || "" })
+
+                data.logs[message.guild.id].actions.push({ "type": "warn", "by": message.author.id, "against": user.id, "for": args[1] || "" })
+
+                fs.writeFileSync("./data.json", JSON.stringify(data, null, 4))
+            })
+        })
+    }
+}
