dd95801b0f9bf54a33828e9fdf34378383969248
Kale-Ko <info@kaleko.ga>
2021-08-07 18:53:08 -0400

    Update

--- commands/management/setconfig.js
+++ commands/management/setconfig.js
@@ -9,19 +9,17 @@ module.exports = {
     requiredPermissions: ["MANAGE_SERVER"],
     worksInDms: false,
     callback: (message, args, client, config) => {
-        if (args.length < 3) { var newMessage = message; newMessage.content = config.prefix + "help " + module.exports.name; runCommand(newMessage, config); return }
-
-        var value = args[2]
+        var value = args[1]
         if (value == "true") value = true
         if (value == "false") value = false
 
-        updateNestedValueOfObj(config, args[1], value)
+        updateNestedValueOfObj(config, args[0], value)
 
         data.configs[message.guild.id] = config
 
         uploadData()
 
-        sendEmbed(message.channel, message.author, config, "Config", "Successfully set " + args[1] + " to " + args[2])
+        sendEmbed(message.channel, message.author, config, "Config", "Successfully set " + args[0] + " to " + args[1])
 
         function updateNestedValueOfObj(data, string, value) {
             let tempObj = data
