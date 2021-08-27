9e55e32817de24df9e296a4036a43816c97a2257
Kale-Ko <github@kaleko.ga>
2021-08-27 16:04:43 -0400

    Update

--- features/mute.js
+++ features/mute.js
@@ -5,6 +5,8 @@ module.exports = {
     description: "Delete messages from a user if they are muted",
     events: ["message"],
     run: (name, message) => {
+        if (message.channel.type == "dm") return
+
         if (data.logs[message.guild.id].mutes[message.author.id] != undefined) message.delete()
     }
 }
